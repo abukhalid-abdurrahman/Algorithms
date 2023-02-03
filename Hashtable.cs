46dfae5a401c8c2a77a4b1844c5692f51e093dd9
Faridun Berdiev <62469844+faha-berdiev@users.noreply.github.com>
2023-02-03 19:52:52 +0500

    Update Hashtable.cs

--- Hashtable.cs
+++ Hashtable.cs
@@ -1,50 +1,82 @@
-﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Algorithms
+var key = "Faridun";
+var value = new Person("Faridun", "Berdiev");
+
+Hashtable<string, Person> hashtable = new Hashtable<string, Person>();
+hashtable[key] = value;
+
+var person = hashtable[key];
+
+Console.WriteLine(person);
+
+record Person(string Name, string Surname);
+
+class HashtableKeyValuePair<K, V>
 {
-    public class HashtableEntry
+    public K Key { get; set; }
+    public V Value { get; set; }
+
+    public HashtableKeyValuePair(K key, V value)
     {
-        public HashtableEntry(object key, object value)
-        {
-            Key = key;
-            Value = value;
-        }
-
-        public object Key { get; set; }
-        public object? Value { get; set; }
+        Key = key;
+        Value = value;
     }
-    
-    public class Hashtable
+}
+
+class Hashtable<K, V> : IEnumerable<HashtableKeyValuePair<K, V>>
+{
+    private readonly int _maxCount = 0;
+    private readonly HashtableKeyValuePair<K, V>[] _keyValuePairArray;
+
+    public Hashtable(int maxCount = 1000)
     {
-        private readonly int _maxCount = 0;
-        private HashtableEntry[] _hashtableEntries;
-            
-        public Hashtable(int maxCount = 1000)
+        _maxCount = maxCount;
+        _keyValuePairArray = new HashtableKeyValuePair<K, V>[_maxCount];
+    }
+
+    public V this[K key]
+    {
+        get => Get(key);
+        set => Put(key, value);
+    }
+
+    public IEnumerator<HashtableKeyValuePair<K, V>> GetEnumerator()
+    {
+        foreach(var pair in _keyValuePairArray)
+            yield return pair;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public V Get(K key)
+    {
+        if(_keyValuePairArray is null || _maxCount == 0)
+            return default;
+
+        var hashCodeIndex = GetKeyHashCode(key);
+        return _keyValuePairArray[hashCodeIndex].Value;
+    }
+
+    public void Put(K key, V value)
+    {
+        if(_keyValuePairArray is null || _maxCount == 0)
+            return;
+        
+        var hashCodeIndex = GetKeyHashCode(key);
+        var hashtableKeyValuePair = _keyValuePairArray[hashCodeIndex];
+        if(hashtableKeyValuePair is null)
         {
-            _maxCount = maxCount;
-            _hashtableEntries = new HashtableEntry[maxCount];
+            _keyValuePairArray[hashCodeIndex] = new HashtableKeyValuePair<K, V>(key, value);
+            return;
         }
 
-        public object? Get(object key)
+        if(EqualityComparer<K>.Default.Equals(hashtableKeyValuePair.Key, key))
         {
-            var hashCodeIndex = Math.Abs(key.GetHashCode() % _maxCount);
-            return _hashtableEntries[hashCodeIndex]?.Value;
-        }
-
-        public void Put(object key, object? value)
-        {
-            var hashCodeIndex = Math.Abs(key.GetHashCode() % _maxCount);
-            var hashTableEntry = _hashtableEntries[hashCodeIndex];
-            if (hashTableEntry == null)
-            {
-                _hashtableEntries[hashCodeIndex] = new HashtableEntry(key, value);
-                return;
-            }
-
-            if (hashTableEntry.Key == key)
-            {
-                hashTableEntry.Value = value;
-            }
+            hashtableKeyValuePair.Value = value;
         }
     }
+
+    private int GetKeyHashCode(K key) => Math.Abs(key.GetHashCode() % _maxCount);   
 }
